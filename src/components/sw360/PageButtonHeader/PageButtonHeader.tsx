41aaa772db63969f181b0a189d428a44ba4761f5
Helio Chissini de Castro <heliocastro@gmail.com>
2023-06-18 13:23:21 +0200

    feat(components): Add PageButtonHeader component to sw360 namespace
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- src/components/sw360/PageButtonHeader/PageButtonHeader.tsx
+++ src/components/sw360/PageButtonHeader/PageButtonHeader.tsx
@@ -1,26 +1,23 @@
-    // Copyright (c) Helio Chissini de Castro, 2023. Part of the SW360 Frontend Project.
+// Copyright (c) Helio Chissini de Castro, 2023. Part of the SW360 Frontend Project.
 
-    // This program and the accompanying materials are made
-    // available under the terms of the Eclipse Public License 2.0
-    // which is available at https://www.eclipse.org/legal/epl-2.0/
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
 
-    // SPDX-License-Identifier: EPL-2.0
-    // License-Filename: LICENSE
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
 
 'use client'
 
+import React from 'react'
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
 import { COMMON_NAMESPACE } from '@/object-types/Constants'
 
+import { PageButtonHeaderProps } from './PageButtonHeader.types'
 import styles from './pagebuttonheader.module.css'
 
-interface Props {
-    title?: string
-    buttons?: { [key: string]: { [key: string]: string } }
-}
-
-function PageButtonHeader({ title, buttons }: Props) {
+function PageButtonHeader({ title, buttons, children }: PageButtonHeaderProps) {
     const t = useTranslations(COMMON_NAMESPACE)
     let buttonList: JSX.Element[] = []
     if (buttons) {
@@ -44,6 +41,7 @@ function PageButtonHeader({ title, buttons }: Props) {
             <div className='col-auto'>
                 <div className='btn-toolbar' role='toolbar'>
                     {buttonList}
+                    {children}
                 </div>
             </div>
             {title && <div className={`col text-truncate ${styles['buttonheader-title']}`}>{title}</div>}
