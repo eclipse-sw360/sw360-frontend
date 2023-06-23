ce1af2998cc0d1174a3195e4549dfd4235f7694a
hoangnt2 <hoang2.nguyenthai@toshiba.co.jp>
2023-06-23 14:55:00 +0700

    fix(code): Add license header and remove unused import

--- src/components/VerificationTooltip/VerificationTooltip.tsx
+++ src/components/VerificationTooltip/VerificationTooltip.tsx
@@ -1,8 +1,17 @@
+// Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
 import { useState } from "react"
 import { VerificationStateInfo } from "@/object-types/LinkedVulnerability"
 import { ReactNode } from 'react'
 import styles from './VerificationTooltip.module.css'
-import { info } from "console"
 
 interface Props {
   verificationStateInfos: Array<VerificationStateInfo>
