86078c56acfc5493fbd6f8be433fa1ea41c82d88
Oberoi <dearsh.oberoi@siemens.com>
2023-07-21 16:24:53 +0530

    fix(License): Add license in each file

--- src/components/sw360/ViewProjects/AdministrationView/Clearing/Clearing.tsx
+++ src/components/sw360/ViewProjects/AdministrationView/Clearing/Clearing.tsx
@@ -1,3 +1,12 @@
+// Copyright (C) Siemens AG, 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
 "use client"
 
 export default function Clearing() {
