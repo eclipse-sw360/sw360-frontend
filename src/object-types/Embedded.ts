a303b2126f44bd824dec84017c84f1981c63ab25
Helio Chissini de Castro <heliocastro@gmail.com>
2023-10-05 19:15:26 +0200

    refactor(component): Add Embedded component as generics
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

---
+++ src/object-types/Embedded.ts
@@ -0,0 +1,39 @@
+// Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Helio Chissini de Castro. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+interface Embedded<SW360Type, SW360Key extends string> {
+    _embedded: {
+        [key in SW360Key]: Array<SW360Type>
+    }
+    _links?: {
+        first?: {
+            href: string
+        }
+        last?: {
+            href: string
+        }
+        curies?: [
+            {
+                href: string
+                name: string
+                templated: boolean
+            }
+        ]
+    }
+    page?: {
+        size?: number
+        totalElements?: number
+        totalPages?: number
+        number?: number
+    }
+}
+
+export default Embedded
