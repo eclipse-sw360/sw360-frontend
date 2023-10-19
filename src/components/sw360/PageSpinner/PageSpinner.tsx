78c5c7bc6572bd61bde8f1dedfcb7c60970ea05a
Helio Chissini de Castro <heliocastro@gmail.com>
2023-10-19 21:19:14 +0200

    feat(component): PageSpinner to show on page loading
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

---
+++ src/components/sw360/PageSpinner/PageSpinner.tsx
@@ -0,0 +1,22 @@
+// Copyright (c) Helio Chissini de Castro, 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+import { Spinner } from 'react-bootstrap'
+
+function PageSpinner() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+            <Spinner className='spinner'>
+                <span className='visually-hidden'>Loading...</span>
+            </Spinner>
+        </div>
+    )
+}
+
+export default PageSpinner
