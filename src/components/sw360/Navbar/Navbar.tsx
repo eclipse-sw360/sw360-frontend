0a117f1b1d2d74ad2b89c1f9f10d04992b3bdb29
deo002 <oberoidearsh@gmail.com>
2023-10-29 11:05:53 +0530

    style(admin): Add admin link on navbar

--- src/components/sw360/Navbar/Navbar.tsx
+++ src/components/sw360/Navbar/Navbar.tsx
@@ -11,17 +11,21 @@
 'use client'
 
 import { LOCALES as locales } from '@/constants'
+import { useSession } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
 import Link from 'next-intl/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useState } from 'react'
-import { Navbar as BSNavbar, Container, Nav } from 'react-bootstrap'
+import { Navbar as BSNavbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
 function Navbar() {
     const [heading, setHeading] = useState('home')
+    const router = useRouter()
     const pathname = usePathname()
     const isLoginPage = locales.includes(pathname.substring(1)) || pathname.substring(1) === ''
     const t = useTranslations('default')
+    const { data: session, status } = useSession()
+    const [show, setShow] = useState(false)
 
     const navlist = [
         { link: '/home', name: t('Home') },
@@ -32,6 +36,7 @@ function Navbar() {
         { link: '/vulnerabilities', name: t('Vulnerabilities') },
         { link: '/requests', name: t('Requests') },
         { link: '/search', name: t('Search') },
+        { link: '/admin', name: t('Admin') },
         { link: '/preferences', name: t('Preferences') },
     ]
 
@@ -39,13 +44,55 @@ function Navbar() {
         <BSNavbar expand='lg'>
             <Container fluid>
                 <Nav variant='underline' className='ms-5' activeKey='home'>
-                    {navlist.map((item) => (
-                        <Nav.Item key={item.name}>
-                            <Link className={`nav-link`} href={item.link} onClick={() => setHeading(item.link[-1])}>
-                                {item.name}
-                            </Link>
-                        </Nav.Item>
-                    ))}
+                    {navlist.map((item) => {
+                        if (item.name === 'Admin') {
+                            if (status === 'authenticated' && session.user.userGroup === 'ADMIN') {
+                                return (
+                                    <NavDropdown
+                                        title={item.name}
+                                        id='admin-dropdown'
+                                        show={show}
+                                        onMouseEnter={() => setShow(true)}
+                                        onMouseLeave={() => setShow(false)}
+                                        key={item.name}
+                                        onClick={(e) => {
+                                            // hack to route to /admin when clicked on dropdown title
+                                            if ((e.target as HTMLElement).attributes[0].name === 'id') {
+                                                e.preventDefault()
+                                                router.push(item.link)
+                                            }
+                                        }}
+                                    >
+                                        <NavDropdown.Item href='#'>{t('User')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Vendors')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Bulk Release Edit')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Licenses')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Obligations')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Schedule')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Fossology')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Import Export')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Database Sanitation')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Attachment Cleanup')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('OAuth Client')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('License Types')}</NavDropdown.Item>
+                                        <NavDropdown.Item href='#'>{t('Department')}</NavDropdown.Item>
+                                    </NavDropdown>
+                                )
+                            }
+                        } else {
+                            return (
+                                <Nav.Item key={item.name}>
+                                    <Link
+                                        className={`nav-link`}
+                                        href={item.link}
+                                        onClick={() => setHeading(item.link[-1])}
+                                    >
+                                        {item.name}
+                                    </Link>
+                                </Nav.Item>
+                            )
+                        }
+                    })}
                 </Nav>
             </Container>
         </BSNavbar>
