dc9c3a21993e12b58654bdcc51f8d0e4721da547
amritkv <er.akverma8@gmail.com>
2024-03-19 12:02:46 +0530

    feat(project_detail): Added feature to generate source code bundle

---
+++ src/app/[locale]/projects/generateSourceCode/[id]/page.tsx
@@ -0,0 +1,27 @@
+// Copyright (C) Siemens AG, 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+import { Metadata } from 'next'
+import GenerateSourceCodeBundle from './components/GenerateSourceCodeBundle'
+
+interface Context {
+    params: { id: string }
+}
+
+export const metadata: Metadata = {
+    title: 'Projects',
+}
+
+const GenerateSourceCode = async ({ params }: Context) => {
+    const projectId = params.id
+
+    return <GenerateSourceCodeBundle projectId={projectId} />
+}
+
+export default GenerateSourceCode
