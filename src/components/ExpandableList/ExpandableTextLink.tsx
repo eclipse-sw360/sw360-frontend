b3b112a81defe6af2156b28ee5e2b57e15d064b7
deo002 <oberoidearsh@gmail.com>
2024-04-11 15:42:51 +0530

    fix(expandable_list): Add ui to preview and  expand long lists

---
+++ src/components/ExpandableList/ExpandableTextLink.tsx
@@ -0,0 +1,33 @@
+// Copyright (C) Siemens AG, 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+'use client'
+
+import { useState } from 'react'
+import { BsCaretDownFill, BsCaretRightFill } from 'react-icons/bs'
+
+export default function ExpandableTextList({ list }: { list: string[] }) {
+    const [ isExpanded, setExpanded ] = useState(false)
+    const fullString = list.join(', ')
+    const previewString = fullString.slice(0, 10).concat('...')
+    return (
+        <>
+            {
+                isExpanded ?
+                <div><BsCaretDownFill onClick={() => setExpanded(false)}/>{' '}{fullString}</div>:
+                <div>
+                    {
+                        list.length !== 0 &&
+                        <div><BsCaretRightFill onClick={() => setExpanded(true)} />{' '}{previewString}</div>
+                    }
+                </div>
+            }
+        </>
+    )
+}
