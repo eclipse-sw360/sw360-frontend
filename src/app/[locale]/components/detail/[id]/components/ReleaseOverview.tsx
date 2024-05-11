5c6d6c8ad521072ee352f98d929ba2e273c075c5
amritkv <er.akverma8@gmail.com>
2024-05-11 18:59:04 +0530

    fix(license_header): Changed year to 2024 in license header

--- src/app/[locale]/components/detail/[id]/components/ReleaseOverview.tsx
+++ src/app/[locale]/components/detail/[id]/components/ReleaseOverview.tsx
@@ -1,6 +1,6 @@
 // Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
 // Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
-// Copyright (C) Siemens AG, 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Siemens AG, 2024. Part of the SW360 Frontend Project.
 
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
