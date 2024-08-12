ab9f58e3e53bfbaa9b2fdf0c9bcf9590c47c880a
amritkv <er.akverma8@gmail.com>
2024-08-12 21:07:46 +0530

    feat(Reopen_CR): Addition of requested clearing date in reopen closed CR dialogue

--- src/app/[locale]/requests/clearingRequest/edit/[id]/components/ReopenClosedClearingRequestModal.tsx
+++ src/app/[locale]/requests/clearingRequest/edit/[id]/components/ReopenClosedClearingRequestModal.tsx
@@ -11,10 +11,11 @@
 
 import { signOut, useSession } from "next-auth/react"
 import { useTranslations } from "next-intl"
-import { Dispatch, SetStateAction, useState } from "react"
-import { Button, Modal } from "react-bootstrap"
-import { BsCheck2Square } from "react-icons/bs"
+import { Dispatch, SetStateAction, useCallback, useState } from "react"
+import { Alert, Button, Col, Form, Modal, Row } from "react-bootstrap"
 import { CreateClearingRequestPayload } from "@/object-types"
+import { FaRegQuestionCircle } from "react-icons/fa"
+import { ShowInfoOnHover } from "next-sw360"
 
 interface Props {
     show?: boolean
@@ -26,36 +27,33 @@ export default function ReopenClosedClearingRequestModal({ show,
                                                          }: Props) {
     const t = useTranslations('default')
     const { status } = useSession()
-    const [comment] = useState('')
-    // const [message] = useState('')
-    // const [variant] = useState('success')
-    const [reloadPage] = useState(false)
+    const [message, setMessage] = useState('')
+    const [variant, setVariant] = useState('success')
+    const [reloadPage, setReloadPage] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false)
     const [showMessage, setShowMessage] = useState(false)
     const [createClearingRequestPayload, setCreateClearingRequestPayload] = 
                                         useState<CreateClearingRequestPayload>({
             requestedClearingDate: '',
-            clearingTeam: '',
             clearingType: '',
             priority: '',
             requestingUserComment: ''
     })
 
-    console.log(createClearingRequestPayload)
-    console.log(showMessage)
+    const handleError = useCallback(() => {
+        displayMessage('danger', t('Error when processing'))
+        setReloadPage(true)
+    }, [t])
 
-    // const handleError = useCallback(() => {
-    //     displayMessage('danger', t('Error when processing'))
-    //     setReloadPage(true)
-    // }, [t])
-
-    // const displayMessage = (variant: string, message: string) => {
-    //     setVariant(variant)
-    //     setMessage(message)
-    //     setShowMessage(true)
-    // }
+    const displayMessage = (variant: string, message: string) => {
+        setVariant(variant)
+        setMessage(message)
+        setShowMessage(true)
+    }
 
     const reopenClearingRequest = () => {
         console.log('reopen closed CR')
+        handleError()
     }
 
     const handleSubmit = () => {
@@ -64,10 +62,10 @@ export default function ReopenClosedClearingRequestModal({ show,
 
     const handleCloseDialog = () => {
         setShow(!show)
+        setIsDisabled(false)
         setShowMessage(false)
         setCreateClearingRequestPayload({
             requestedClearingDate: '',
-            clearingTeam: '',
             clearingType: '',
             priority: '',
             requestingUserComment: ''
@@ -75,6 +73,15 @@ export default function ReopenClosedClearingRequestModal({ show,
         if (reloadPage === true) {
             window.location.reload()
         }
+    }
+
+    const updateInputField = (event: React.ChangeEvent<HTMLSelectElement |
+                                                       HTMLInputElement |
+                                                       HTMLTextAreaElement>) => {
+        setCreateClearingRequestPayload({
+            ...createClearingRequestPayload,
+            [event.target.name]: event.target.value,
+        })
     }
     
 
@@ -95,14 +102,57 @@ export default function ReopenClosedClearingRequestModal({ show,
                 >
                     <Modal.Header closeButton style={{ color: '#2E5AAC' }}>
                         <Modal.Title id='reopen-clearing-request-modal'>
-                            <BsCheck2Square style={{ marginBottom: '5px',
+                            <FaRegQuestionCircle style={{ marginBottom: '5px',
                                                      color: '#2E5AAC',
-                                                     fontSize: '23px'}} />
+                                                     fontSize: '19px'}} />
                                 {' '}
                                 { t('reopen clearing request')}
                         </Modal.Title>
                     </Modal.Header>
-
+                    <Modal.Body>
+                        <Alert variant={variant}
+                            onClose={() => setShowMessage(false)}
+                            show={showMessage}
+                            dismissible>
+                            {message}
+                        </Alert>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label className='mb-1'>
+                                    {t('Please enter details to re-open clearing request')}
+                                </Form.Label>
+                                <br />
+                            </Form.Group>
+                            <hr />
+                            <Row className='mb-3'>                                
+                                <Col md={6}>
+                                    <Form.Group className='mb-2'>
+                                        <Form.Label style={{ fontWeight: 'bold' }}>
+                                            {t('Preferred Clearing Date')} :
+                                            <span className='text-red'
+                                                  style={{ color: '#F7941E' }}>
+                                                *
+                                            </span>
+                                        </Form.Label>
+                                        <Form.Control
+                                            type='date'
+                                            id='createClearingRequest.requestedClearingDate'
+                                            name='requestedClearingDate'
+                                            value={createClearingRequestPayload?.requestedClearingDate ?? ''}
+                                            onChange={updateInputField}
+                                            disabled={isDisabled}
+                                            required
+                                        />
+                                        <div className='form-text'
+                                             id='createClearingRequest.requestedClearingDate.HelpBlock'>
+                                            <ShowInfoOnHover text={t('Requested Clearing Date Info')}/>
+                                                {' '}{t('Learn more about preferred clearing date')}.
+                                        </div>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Modal.Body>
                     <Modal.Footer className='justify-content-end'>
                         <Button className='btn-secondary'
                                 variant='light'
@@ -113,7 +163,8 @@ export default function ReopenClosedClearingRequestModal({ show,
                         <Button
                             className='login-btn'
                             variant='primary'
-                            disabled={!comment}
+                            disabled={!createClearingRequestPayload.clearingType ||
+                                      !createClearingRequestPayload.requestedClearingDate}
                             onClick={() => handleSubmit()}
                             hidden={reloadPage}
                         >
