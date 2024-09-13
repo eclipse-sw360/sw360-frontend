36a152bc4e6060b75a36a1830bff34b7a096d3fa
amritkv <er.akverma8@gmail.com>
2024-09-13 10:28:35 +0530

    refactor(CR): Refactored epoch to date-time calculation

--- src/app/[locale]/requests/clearingRequest/detail/[id]/components/ClearingComments.tsx
+++ src/app/[locale]/requests/clearingRequest/detail/[id]/components/ClearingComments.tsx
@@ -40,14 +40,8 @@ export default function ClearingComments({ clearingRequestId }:
             return null
         }
         const date = new Date(timestamp)
-        const year = date.getFullYear()
-        const month = String(date.getMonth() + 1).padStart(2, '0')
-        const day = String(date.getDate()).padStart(2, '0')
-        const hours = String(date.getHours()).padStart(2, '0')
-        const minutes = String(date.getMinutes()).padStart(2, '0')
-        const seconds = String(date.getSeconds()).padStart(2, '0')
-    
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+        const dateISOString = date.toISOString()
+        return `${dateISOString.slice(0,10)}  ${dateISOString.slice(11,19)}`
     }
 
     const fetchData = useCallback(
@@ -102,7 +96,6 @@ export default function ClearingComments({ clearingRequestId }:
             return signOut()
         } else {
             MessageService.error(t('There are some problem to update your comments'))
-            notFound()
         }
     }
 
