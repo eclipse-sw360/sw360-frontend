cbf9e6a428bb7eaea7bc5a20760f457717e982d2
Helio Chissini de Castro <heliocastro@gmail.com>
2024-11-27 08:17:11 +0100

    fix: Small next auth linter fix
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -9,30 +9,30 @@
 // SPDX-License-Identifier: EPL-2.0
 // License-Filename: LICENSE
 
-import { AUTH_PROVIDER } from '@/utils/env';
-import keycloakAuth from './keycloakAuthOption';
-import { NextAuthOptions } from 'next-auth';
-import basicAuthOption from './basicAuthOption';
-import sw360OauthOption from './sw360OauthOption';
-import sw360OauthPwdGrantTypeOption from './sw360OauthPwdGrantTypeOption';
+import { AUTH_PROVIDER } from '@/utils/env'
+import { NextAuthOptions } from 'next-auth'
+import basicAuthOption from './basicAuthOption'
+import keycloakAuth from './keycloakAuthOption'
+import sw360OauthOption from './sw360OauthOption'
+import sw360OauthPwdGrantTypeOption from './sw360OauthPwdGrantTypeOption'
 
-let authOptions: NextAuthOptions
-
+let provider: NextAuthOptions
 switch (AUTH_PROVIDER) {
     case 'sw360oauth':
-        authOptions = sw360OauthOption
-        break;
+        provider = sw360OauthOption
+        break
     case 'keycloak':
-        authOptions = keycloakAuth
-        break;
+        provider = keycloakAuth
+        break
     case 'oauth-password-grant':
-        authOptions = sw360OauthPwdGrantTypeOption
-        break;
+        provider = sw360OauthPwdGrantTypeOption
+        break
     case 'sw360basic':
     default:
-        authOptions = basicAuthOption
-        break;
+        provider = basicAuthOption
+        break
 }
 
-export default authOptions
+const authOptions: NextAuthOptions = provider
 
+export default authOptions
