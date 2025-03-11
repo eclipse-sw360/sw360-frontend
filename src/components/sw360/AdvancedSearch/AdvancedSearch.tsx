026f7e7dd33d34166574a797558df492961d6bc3
harith-hacky03 <harithhacky3@gmail.com>
2025-03-11 21:23:17 +0530

    chore(Project): Preserved Advanced Search Result

--- src/components/sw360/AdvancedSearch/AdvancedSearch.tsx
+++ src/components/sw360/AdvancedSearch/AdvancedSearch.tsx
@@ -1,17 +1,8 @@
-// Copyright (c) Helio Chissini de Castro, 2023. Part of the SW360 Frontend Project.
-
-// This program and the accompanying materials are made
-// available under the terms of the Eclipse Public License 2.0
-// which is available at https://www.eclipse.org/legal/epl-2.0/
-
-// SPDX-License-Identifier: EPL-2.0
-// License-Filename: LICENSE
-
 'use client'
 
 import { useTranslations } from 'next-intl'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { useState, type JSX } from 'react';
+import React, { useState, type JSX } from 'react'
 import { Button, Form } from 'react-bootstrap'
 
 import CommonUtils from '@/utils/common.utils'
@@ -68,25 +59,105 @@ function AdvancedSearch({ title = 'Advanced Search', fields }: Props): JSX.Eleme
         router.push(encodedUrl)
     }
 
-    let fieldList: JSX.Element[] = []
-    if (fields) {
-        fieldList = fields.map((field: Field) => {
-            const fieldLabel = field.fieldName
-            if (field.paramName === 'createdOn' && Array.isArray(field.value)) {
-                return (
-                    <div key='createdOn'>
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        submitSearch()
+    }
+
+    const renderField = (field: Field) => {
+        const fieldLabel = field.fieldName
+        switch (field.paramName) {
+            case 'createdOn':
+                if (Array.isArray(field.value)) {
+                    return (
+                        <div key='createdOn'>
+                            <Form.Group className='mb-3' controlId={field.paramName}>
+                                <Form.Label className='label'>{fieldLabel}</Form.Label>
+                                <Form.Select
+                                    aria-label={field.fieldName}
+                                    className='form-control'
+                                    size='sm'
+                                    name={field.paramName}
+                                    onChange={changeCreatedOnSearchOption}
+                                    value={searchParams.type}
+                                >
+                                    <option value='' />
+                                    {field.value.map((option) => (
+                                        <option key={option.key} value={option.key}>
+                                            {option.text}
+                                        </option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+
+                            {createdOnSearchOption !== 'BETWEEN' && createdOnSearchOption !== '' && (
+                                <Form.Group className='mb-3'>
+                                    <Form.Control 
+                                        type='date' 
+                                        size='sm' 
+                                        name='createdOnDate'
+                                        value={searchParams.createdOnDate}
+                                        onChange={handleSearchParam}
+                                    />
+                                </Form.Group>
+                            )}
+
+                            {createdOnSearchOption === 'BETWEEN' && (
+                                <>
+                                    <Form.Group className='mb-3'>
+                                        <Form.Control 
+                                            type='date' 
+                                            size='sm' 
+                                            name='createdOnStart'
+                                            value={searchParams.createdOnStart}
+                                            onChange={handleSearchParam}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className='mb-3'>
+                                        <Form.Label className='label'>{t('To')}</Form.Label>
+                                        <Form.Control 
+                                            type='date' 
+                                            size='sm' 
+                                            name='createdOnEnd'
+                                            value={searchParams.createdOnEnd}
+                                            onChange={handleSearchParam}
+                                        />
+                                    </Form.Group>
+                                </>
+                            )}
+                        </div>
+                    )
+                }
+                break
+
+            default:
+                if (typeof field.value === 'string') {
+                    return (
+                        <Form.Group key={field.paramName} className='mb-3' controlId={field.paramName}>
+                            <Form.Label className='label'>{fieldLabel}</Form.Label>
+                            <Form.Control
+                                className='form-control'
+                                type='text'
+                                size='sm'
+                                name={field.paramName}
+                                value={searchParams[field.paramName] || ''}
+                                onChange={handleSearchParam}
+                            />
+                        </Form.Group>
+                    )
+                }
+                if (Array.isArray(field.value)) {
+                    return (
                         <Form.Group key={field.paramName} className='mb-3' controlId={field.paramName}>
                             <Form.Label className='label'>{fieldLabel}</Form.Label>
                             <Form.Select
-                                aria-label={field.fieldName}
-                                title={'blob'}
                                 className='form-control'
                                 size='sm'
                                 name={field.paramName}
-                                onChange={changeCreatedOnSearchOption}
-                                value={searchParams.type}
+                                onChange={handleSearchParam}
+                                value={searchParams[field.paramName] || ''}
                             >
-                                <option defaultValue='' />
+                                <option value='' />
                                 {field.value.map((option) => (
                                     <option key={option.key} value={option.key}>
                                         {option.text}
@@ -94,65 +165,11 @@ function AdvancedSearch({ title = 'Advanced Search', fields }: Props): JSX.Eleme
                                 ))}
                             </Form.Select>
                         </Form.Group>
-
-                        {createdOnSearchOption !== 'BETWEEN' && createdOnSearchOption !== '' && (
-                            <Form.Group className='mb-3'>
-                                <Form.Control type='date' size='sm' />
-                            </Form.Group>
-                        )}
-
-                        {createdOnSearchOption === 'BETWEEN' && (
-                            <>
-                                <Form.Group className='mb-3'>
-                                    <Form.Control type='date' size='sm' />
-                                </Form.Group>
-                                <Form.Group className='mb-3'>
-                                    <Form.Label className='label'>{t('To')}</Form.Label>
-                                    <Form.Control type='date' size='sm' />
-                                </Form.Group>
-                            </>
-                        )}
-                    </div>
-                )
-            } else if (typeof field.value === 'string') {
-                return (
-                    <Form.Group key={field.paramName} className='mb-3' controlId={field.paramName}>
-                        <Form.Label className='label'>{fieldLabel}</Form.Label>
-                        <Form.Control
-                            className='form-control'
-                            type='text'
-                            size='sm'
-                            name={field.paramName}
-                            value={searchParams[field.paramName]}
-                            onChange={handleSearchParam}
-                            onKeyDown={(e) => e.key == "Enter" && submitSearch()}
-                        />
-                    </Form.Group>
-                )
-            } else if (Array.isArray(field.value)) {
-                return (
-                    <Form.Group key={field.paramName} className='mb-3' controlId={field.paramName}>
-                        <Form.Label className='label'>{fieldLabel}</Form.Label>
-                        <Form.Select
-                            className={'form-control'}
-                            size='sm'
-                            name={field.paramName}
-                            onChange={handleSearchParam}
-                            value={searchParams.type}
-                        >
-                            <option defaultValue='' />
-                            {field.value.map((option) => (
-                                <option key={option.key} value={option.key}>
-                                    {option.text}
-                                </option>
-                            ))}
-                        </Form.Select>
-                    </Form.Group>
-                )
-            } else {
-                return <></>
-            }
-        })
+                    )
+                }
+                break
+        }
+        return null
     }
 
     return (
@@ -161,26 +178,37 @@ function AdvancedSearch({ title = 'Advanced Search', fields }: Props): JSX.Eleme
                 <div className='card-header'>{title}</div>
 
                 <div className='card-body'>
-                    <Form>{fieldList}</Form>
-                    <Form.Group className='mb-3'>
-                        <Form.Check
-                            type='checkbox'
-                            label={t('Exact Match')}
-                            placeholder='Exact Match'
-                            style={{
-                                fontWeight: 'bold',
-                                fontSize: '14px',
-                                display: 'inline-block',
-                                marginRight: '5px',
-                            }}
-                        />
-                        <ShowInfoOnHover text={t('Exact_Match_Info')} />
-                    </Form.Group>
-                    <Form.Group>
-                        <Button size='sm' variant='primary' className='w-100' onClick={submitSearch}>
-                            {t('Search')}
-                        </Button>
-                    </Form.Group>
+                    <Form onSubmit={handleSubmit}>
+                        {fields?.map((field) => renderField(field))}
+
+                        <Form.Group className='mb-3'>
+                            <Form.Check
+                                type='checkbox'
+                                label={t('Exact Match')}
+                                id='exactMatch'
+                                checked={searchParams.exactMatch === 'true'}
+                                onChange={(e) => 
+                                    setSearchParam(prev => ({
+                                        ...prev,
+                                        exactMatch: e.target.checked ? 'true' : ''
+                                    }))
+                                }
+                                style={{
+                                    fontWeight: 'bold',
+                                    fontSize: '14px',
+                                    display: 'inline-block',
+                                    marginRight: '5px',
+                                }}
+                            />
+                            <ShowInfoOnHover text={t('Exact_Match_Info')} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Button type='submit' size='sm' variant='primary' className='w-100'>
+                                {t('Search')}
+                            </Button>
+                        </Form.Group>
+                    </Form>
                 </div>
             </div>
         </div>
