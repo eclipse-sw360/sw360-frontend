66bcfa45a2a6e7f6d335579a341111e53151634e
Govind Gupta <202152312@iiitvadodara.ac.in>
2025-03-17 18:08:16 +0530

    feat(license-clearing): Added filters for Type, State, and Relation

--- src/components/sw360/TreeTable/TreeTable.tsx
+++ src/components/sw360/TreeTable/TreeTable.tsx
@@ -16,14 +16,15 @@
 
 'use client'
 
-import React, { useEffect, useState, type JSX } from 'react';
+import React, { useEffect, useState, type JSX } from 'react'
+import { Dropdown } from 'react-bootstrap'
 import { BsCaretDownFill, BsCaretRightFill } from 'react-icons/bs'
 
 import { NodeData } from '@/object-types'
-import { Table, _ } from 'next-sw360'
 import { OneDArray, TColumn } from 'gridjs/dist/src/types'
-import { ComponentChild } from 'preact'
 import { SearchConfig } from 'gridjs/dist/src/view/plugin/search/search'
+import { Table, _ } from 'next-sw360'
+import { ComponentChild } from 'preact'
 import { Language } from '../Table/Table'
 
 interface Props {
@@ -45,12 +46,22 @@ const PaddedCell: React.FC<Props> = ({
 }) => {
     return (
         <div className='d-flex'>
-            <span className='indenter' style={{paddingLeft: `${padLength * 1}rem`}} role='button'>
+            <span
+                className='indenter'
+                style={{ paddingLeft: `${padLength * 1}rem` }}
+                role='button'
+            >
                 {parent &&
                     (needExpand ? (
-                        <BsCaretDownFill color='gray' onClick={collapseRow} />
+                        <BsCaretDownFill
+                            color='gray'
+                            onClick={collapseRow}
+                        />
                     ) : (
-                        <BsCaretRightFill color='gray' onClick={expandRow} />
+                        <BsCaretRightFill
+                            color='gray'
+                            onClick={expandRow}
+                        />
                     ))}{' '}
             </span>
             {children}
@@ -58,10 +69,21 @@ const PaddedCell: React.FC<Props> = ({
     )
 }
 
+// Define the structure for filter props
+interface FilterProps {
+    enabled: boolean
+    options: string[]
+    onChange: (value: string) => void
+}
+
+interface ColumnWithFilter extends TColumn {
+    filter?: FilterProps
+}
+
 interface TreeTableProps {
     data: Array<any>
     setData: React.Dispatch<React.SetStateAction<Array<any>>>
-    columns: OneDArray<TColumn | string | ComponentChild>
+    columns: OneDArray<ColumnWithFilter | string | ComponentChild>
     onExpand?: (item: NodeData) => void
     search?: SearchConfig | boolean
     language?: Language
@@ -69,8 +91,82 @@ interface TreeTableProps {
     sort?: boolean
 }
 
-const TreeTable = ({ data, setData, columns, onExpand, search, language, selector, sort }: TreeTableProps): JSX.Element => {
+// Custom header component to include filter dropdown
+const FilterHeader = ({ column, t }: { column: ColumnWithFilter; t?: (key: string) => string }) => {
+    const translate = t || ((key: string) => key)
+
+    if (column.filter?.enabled) {
+        return (
+            <div className='d-flex align-items-center justify-content-between'>
+                <span>{column.name}</span>
+                <Dropdown>
+                    <Dropdown.Toggle
+                        variant='link'
+                        size='sm'
+                        className='p-0 ms-2'
+                    >
+                        <span className='filter-icon'>
+                            {/* Unicode character for filter or checkmark inside a square */}
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='16'
+                                height='16'
+                                fill='white'
+                                className='bi bi-check-square'
+                                viewBox='0 0 16 16'
+                            >
+                                <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z' />
+                                <path d='M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z' />
+                            </svg>
+                        </span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item
+                            key='all'
+                            onClick={() => column.filter?.onChange('')}
+                        >
+                            {translate('All')}
+                        </Dropdown.Item>
+                        {column.filter.options.map((option) => (
+                            <Dropdown.Item
+                                key={option}
+                                onClick={() => column.filter?.onChange(option)}
+                            >
+                                {option}
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+        )
+    }
+
+    return <>{column.name}</>
+}
+
+const TreeTable = ({
+    data,
+    setData,
+    columns,
+    onExpand,
+    search,
+    language,
+    selector,
+    sort,
+}: TreeTableProps): JSX.Element => {
     const [tabledata, setTableData] = useState<any[]>([])
+
+    // Process columns to add filter headers
+    const processedColumns = columns.map((column: any) => {
+        if (column.filter?.enabled) {
+            return {
+                ...column,
+                name: _(<FilterHeader column={column} />),
+            }
+        }
+        return column
+    })
+
     useEffect(() => {
         const newData: any[] = []
         data.forEach((item: NodeData) => {
@@ -109,8 +205,8 @@ const TreeTable = ({ data, setData, columns, onExpand, search, language, selecto
                                 collapseRow={() => collapseRow(item)}
                             >
                                 {cell}
-                            </PaddedCell>
-                        )
+                            </PaddedCell>,
+                        ),
                     )
                 } else {
                     parsedRowData.push(_(<PaddedCell padLength={level}>{cell}</PaddedCell>))
@@ -135,6 +231,17 @@ const TreeTable = ({ data, setData, columns, onExpand, search, language, selecto
         }
     }
 
-    return <Table data={tabledata} pagination={false} columns={columns} sort={sort} search={search} language={language} selector={selector}/>
+    return (
+        <Table
+            data={tabledata}
+            pagination={false}
+            columns={processedColumns}
+            sort={sort}
+            search={search}
+            language={language}
+            selector={selector}
+        />
+    )
 }
+
 export default TreeTable
