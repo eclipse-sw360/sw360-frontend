61191065aa281f1d43b497908548a042136e9891
VAIBHAVSING <vpatil5212@gmail.com>
2025-03-26 18:48:29 +0530

    fix(home):Added license header at error.tsx
    Signed-off-by: VAIBHAVSING <vpatil5212@gmail.com>

--- src/app/[locale]/home/error.tsx
+++ src/app/[locale]/home/error.tsx
@@ -1,3 +1,12 @@
+// Copyright (c) VAIBHAVSING, 2025. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
 "use client"
 
 import { useState, useEffect } from 'react'
