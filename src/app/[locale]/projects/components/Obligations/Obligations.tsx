75c7f416ffa8ec0c9c5bbd308c357937e464d128
amritkv <er.akverma8@gmail.com>
2025-03-28 13:43:31 +0530

    feat(Project): Added feature to create project clearing report

--- src/app/[locale]/projects/components/Obligations/Obligations.tsx
+++ src/app/[locale]/projects/components/Obligations/Obligations.tsx
@@ -17,17 +17,23 @@ import ObligationView from './ObligationsView/ObligationsView'
 import ReleaseView from './ReleaseView'
 import { ActionType, ProjectObligation } from '@/object-types'
 import CompareObligation from './CompareObligation'
+import { useRouter } from 'next/navigation'
 
 export default function Obligations({ projectId, actionType, payload, setPayload }: 
     { projectId: string, actionType: ActionType, payload?: ProjectObligation, setPayload?: Dispatch<SetStateAction<ProjectObligation>> }): JSX.Element {
+    const router = useRouter()
     const t = useTranslations('default')
     const [key, setKey] = useState('obligations-view')
     const [show, setShow] = useState(false)
     const [selectedProjectId, setSelectedProjectId] = useState<string | null>(null)
 
+    const generateLicenseInfo = (withSubProjects: boolean) => {
+        router.push(`/projects/generateLicenseInfo/${projectId}?withSubProjects=${withSubProjects}`)
+    }
+
     return (
         <>
-        <CompareObligation show={show} setShow={setShow} setSelectedProjectId={setSelectedProjectId} />
+            <CompareObligation show={show} setShow={setShow} setSelectedProjectId={setSelectedProjectId} />
             <Tab.Container id='views-tab' activeKey={key} onSelect={(k) => setKey(k as string)}>
                 <div className='row'>
                     <div className='col ps-0'>
@@ -47,10 +53,20 @@ export default function Obligations({ projectId, actionType, payload, setPayload
                     {
                         (actionType === ActionType.DETAIL) &&
                         <Dropdown className='col-auto'>
-                            <Dropdown.Toggle variant='primary'>{t('Create Project Clearing Report')}</Dropdown.Toggle>
+                            <Dropdown.Toggle variant='primary'>
+                                {t('Create Project Clearing Report')}
+                            </Dropdown.Toggle>
                             <Dropdown.Menu>
-                                <Dropdown.Item eventKey={key}>{t('Project only')}</Dropdown.Item>
-                                <Dropdown.Item eventKey={key}>{t('Project with sub project')}</Dropdown.Item>
+                                <Dropdown.Item
+                                    onClick = {() => generateLicenseInfo(false)}
+                                >
+                                    {t('Projects only')}
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                    onClick = {() => generateLicenseInfo(true)}
+                                >
+                                    {t('Projects with sub projects')}
+                                </Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                     }
