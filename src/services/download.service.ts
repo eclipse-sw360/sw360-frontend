d5257ac1d79435f5a8eab3c227fabc69de57cd71
amritkv <er.akverma8@gmail.com>
2025-04-07 14:21:12 +0530

    fix(Service): Added feature to return optional response status at download service

--- src/services/download.service.ts
+++ src/services/download.service.ts
@@ -1,5 +1,6 @@
 // Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
 // Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Siemens AG, 2025. Part of the SW360 Frontend Project.
 
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
@@ -11,22 +12,31 @@
 import { ApiUtils, CommonUtils } from '@/utils'
 import { Session } from 'next-auth'
 
-const download = (url: string, session: Session | null, fileName: string): void => {
+const download = async (url: string,
+                        session: Session | null,
+                        fileName: string) : Promise<number | undefined> => {
     if (CommonUtils.isNullOrUndefined(session)) {
-        console.error('User is undefined');
-        return;
+        console.error('User is undefined')
+        return
     }
-    ApiUtils.GET(url, session.user.access_token)
-        .then((response) => response.blob())
-        .then((blob: Blob) => {
-            const objectURL = URL.createObjectURL(blob)
-            const link = document.createElement('a')
-            link.href = objectURL
-            link.setAttribute('download', fileName)
-            link.click()
-            setTimeout(() => window.URL.revokeObjectURL(objectURL), 0)
-        })
-        .catch((error) => console.log('error', error))
+    try {
+        const response = await ApiUtils.GET(url, session.user.access_token)
+        if (!response.ok) {
+          console.error('Failed to download file:', response.status)
+          return response.status
+        }
+        const blob = await response.blob()
+        const objectURL = URL.createObjectURL(blob)
+        const link = document.createElement('a')
+        link.href = objectURL
+        link.setAttribute('download', fileName)
+        link.click()
+        setTimeout(() => window.URL.revokeObjectURL(objectURL), 0)
+        return response.status
+      } catch (error) {
+        console.log('error', error)
+        return
+      }
 }
 
 const DownloadService = {
