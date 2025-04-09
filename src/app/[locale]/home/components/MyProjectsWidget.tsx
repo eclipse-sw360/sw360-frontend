0f2fcf24cc88ab5ad44803d74794334d0b2a0f5b
VAIBHAVSING <vpatil5212@gmail.com>
2025-04-09 22:27:31 +0530

    fix(Home):Incorporate Approved Release column at My Projects table
    Signed-off-by: VAIBHAVSING <vpatil5212@gmail.com>

--- src/app/[locale]/home/components/MyProjectsWidget.tsx
+++ src/app/[locale]/home/components/MyProjectsWidget.tsx
@@ -9,7 +9,7 @@
 
 import { getSession, signOut } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams, notFound } from 'next/navigation'
 import { ReactNode, useCallback, useEffect, useState, type JSX } from 'react';
 import { Spinner } from 'react-bootstrap'
 
@@ -23,6 +23,61 @@ import { Embedded, Project } from '@/object-types'
 import HomeTableHeader from './HomeTableHeader'
 
 type EmbeddedProjects = Embedded<Project, 'sw360:projects'>
+
+interface LicenseClearingData {
+    'Release Count': number
+    'Approved Count': number
+}
+
+function LicenseClearing({ projectId }: { projectId: string }) {
+    const [lcData, setLcData] = useState<LicenseClearingData | null>(null)
+    useEffect(() => {
+        const controller = new AbortController()
+        const signal = controller.signal
+        void (async () => {
+            try {
+                const session = await getSession()
+                if (!session) {
+                    return signOut()
+                }
+
+                const response = await ApiUtils.GET(
+                    `projects/${projectId}/licenseClearingCount`,
+                    session.user.access_token,
+                    signal
+                )
+                if (response.status === HttpStatus.UNAUTHORIZED) {
+                    return signOut()
+                } else if (response.status !== HttpStatus.OK) {
+                    return notFound()
+                }
+
+                const data = await response.json() as LicenseClearingData
+
+                setLcData(data)
+            } catch (e) {
+                if (e instanceof Error) {
+                    throw new Error(e.message)
+                } else {
+                    throw new Error('Unknown error')
+                }
+            }
+        })()
+        return () => controller.abort()
+    }, [])
+
+    return (
+        <>
+            {lcData ? (
+                <div className='text-center'>{`${lcData['Approved Count']}/${lcData['Release Count']}`}</div>
+            ) : (
+                <div className='col-12 text-center'>
+                    <Spinner className='spinner' size='sm' />
+                </div>
+            )}
+        </>
+    )
+}
 
 function MyProjectsWidget(): ReactNode {
     const [data, setData] = useState<Array<(string | JSX.Element)[]>>([])
@@ -60,17 +115,17 @@ function MyProjectsWidget(): ReactNode {
                 if (!CommonUtils.isNullOrUndefined(projects['_embedded']['sw360:projects'])) {
                     setData(
                         projects['_embedded']['sw360:projects'].map((item: Project) => [
-                            `${item.name }|${CommonUtils.getIdFromUrl(item._links.self.href)}|${CommonUtils.isNullEmptyOrUndefinedString(item.version) && `(${item.version})`}`,
+                            `${item.name}|${CommonUtils.getIdFromUrl(item._links.self.href)}|${CommonUtils.isNullEmptyOrUndefinedString(item.version) && `(${item.version})`}`,
                             CommonUtils.truncateText(item.description ?? '', 40),
-                            item.version ?? '',
+                            item.id ?? '',
                         ]),
                     )
                 }
             })
-            .catch((err:Error) => {
+            .catch((err: Error) => {
                 throw new Error(err.message)
             })
-            .finally(()=>{
+            .finally(() => {
                 setLoading(false)
             })
 
@@ -95,7 +150,15 @@ function MyProjectsWidget(): ReactNode {
             },
         },
         t('Description'),
-        t('Version'),
+        {
+            id: 'License Clearing',
+            name: t('License Clearing'),
+            formatter: (cell: string) => {
+                return _(
+                    <LicenseClearing projectId={cell} />
+                );
+            }
+        }
     ]
     const language = { noRecordsFound: t('NoProjectsFound') }
 
