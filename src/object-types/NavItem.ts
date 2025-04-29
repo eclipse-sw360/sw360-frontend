e98a32a94606cfd936118186ff086738a2bb0bf3
amritkv <er.akverma8@gmail.com>
2025-04-29 15:37:54 +0530

    feat(Admin): Added SW360_ADMIN role to access admin tab

--- src/object-types/NavItem.ts
+++ src/object-types/NavItem.ts
@@ -1,4 +1,5 @@
 // Copyright (C) Helio Chissini de Castro, 2024. Part of the SW360 Frontend Project.
+// Copyright (C) Siemens AG, 2025. Part of the SW360 Frontend Project.
 
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
@@ -7,11 +8,13 @@
 // SPDX-License-Identifier: EPL-2.0
 // License-Filename: LICENSE
 
+import UserGroupType from "./enums/UserGroupType"
+
 type NavItem = {
     href: string
     name: string
     id: string
-    visibility?: string
+    visibility?: UserGroupType[]
     childs?: Array<NavItem>
 }
 
