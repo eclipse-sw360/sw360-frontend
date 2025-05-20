b25b4e80943d3079a34ac881b96049d557e046e3
amritkv <er.akverma8@gmail.com>
2025-05-20 15:39:45 +0530

    feat(Admin): Feature to fetch default fossology connection status

--- src/app/[locale]/admin/fossology/components/FossologyOverview.tsx
+++ src/app/[locale]/admin/fossology/components/FossologyOverview.tsx
@@ -9,11 +9,49 @@
 
 'use client'
 
+import { HttpStatus } from '@/object-types'
+import CommonUtils from '@/utils/common.utils'
+import { ApiUtils } from '@/utils/index'
+import { getSession, signOut } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
-import { ReactNode } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
+
+enum FossologyStatus {
+    SUCCESS = 'Success',
+    FAILURE = 'Failure',
+}
 
 export default function FossologyOverview() : ReactNode {
     const t = useTranslations('default')
+    const [fossologyStatus, setFossologyStatus] = useState<FossologyStatus>(FossologyStatus.FAILURE)
+    const [toggleGeneralInformation, setToggleGeneralInformation] = useState(false)
+
+
+    const fetchData = useCallback(async (url: string) => {
+        const session = await getSession()
+        if (CommonUtils.isNullOrUndefined(session)) return signOut()
+        const response = await ApiUtils.GET(url, session.user.access_token)
+        if (response.status === HttpStatus.OK) {
+            return HttpStatus.OK
+        } else if (response.status === HttpStatus.UNAUTHORIZED) {
+            return signOut()
+        } else {
+            return undefined
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchData('fossology/reServerConnection')
+            .then((response: number | undefined) => {
+                if (response === HttpStatus.OK) {
+                    setFossologyStatus(FossologyStatus.SUCCESS)
+                }
+                else {
+                    setFossologyStatus(FossologyStatus.FAILURE)
+                }
+            })
+            .catch((err) => console.error(err))
+        }, [fetchData])
 
 
     return (
@@ -38,6 +76,40 @@ export default function FossologyOverview() : ReactNode {
                         {t('Fossology Connection Administration')}
                     </div>
                 </div>
+                <table className='table summary-table mt-4'>
+                    <thead
+                        title='Click to expand or collapse'
+                        onClick={() => {
+                            setToggleGeneralInformation(!toggleGeneralInformation)
+                        }}
+                    >
+                        <tr>
+                            <th colSpan={3}>{t('Connection Status')}</th>
+                        </tr>
+                    </thead>
+                    <tbody hidden={toggleGeneralInformation}>
+                        <tr>
+                            <td>{t('Connection to FOSSology is currently in state')}:</td>
+                            <td>
+                                {
+                                    fossologyStatus === 'Success' ?
+                                        <span className='badge bg-success capsule-right'
+                                            style={{ fontSize: '0.8rem' }}>
+                                            {t(`${fossologyStatus}`)}
+                                        </span>
+                                    :
+                                        <span className='badge bg-danger capsule-right'
+                                            style={{ fontSize: '0.8rem' }}>
+                                            {t(`${fossologyStatus}`)}
+                                        </span>
+                                }
+                            </td>
+                            <td>
+                                {t('checked on saved configuration')}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
