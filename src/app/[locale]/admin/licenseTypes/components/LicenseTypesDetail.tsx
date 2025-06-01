94a07e7ad50078fdbb25974ce4aae22091005283
amritkv <er.akverma8@gmail.com>
2025-06-01 12:37:48 +0530

    feat(Admin): Boiler plate code for license types page

---
+++ src/app/[locale]/admin/licenseTypes/components/LicenseTypesDetail.tsx
@@ -0,0 +1,22 @@
+
+// Copyright (C) Siemens AG, 2025. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+'use client'
+
+import { ReactNode } from 'react'
+
+export default function LicenseTypesDetail() : ReactNode {
+
+
+    return (
+        <>
+        </>
+    )
+}
