dff8c69db1245a16f9911ed651acde34f551e3b6
Helio Chissini de Castro <heliocastro@gmail.com>
2025-06-09 11:23:29 +0200

    refactor: Use proper eslint 9.x setup with flat config
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- src/components/sw360/GlobalMessages/index.tsx
+++ src/components/sw360/GlobalMessages/index.tsx
@@ -10,20 +10,20 @@
 
 'use client'
 
-import { useState, useEffect, useRef, type JSX } from 'react';
-import { usePathname } from 'next/navigation'
-import MessageService from '@/services/message.service'
 import { Message } from '@/object-types'
+import MessageService from '@/services/message.service'
+import { CommonUtils } from '@/utils'
+import { usePathname } from 'next/navigation'
+import { useEffect, useRef, useState, type JSX } from 'react'
 import { Alert } from 'react-bootstrap'
 import { FaInfoCircle } from 'react-icons/fa'
 import { IoMdCheckmarkCircle } from 'react-icons/io'
-import { CommonUtils } from '@/utils'
 
 interface Props {
     id?: string
 }
 
-const MessageIcon = ( { messageType }: { messageType: string | undefined }) => {
+const MessageIcon = ({ messageType }: { messageType: string | undefined }) => {
     switch (messageType) {
         case 'success':
             return <IoMdCheckmarkCircle />
@@ -34,7 +34,7 @@ const MessageIcon = ( { messageType }: { messageType: string | undefined }) => {
     }
 }
 
-function GlobalMessages({ id = 'default-message'}: Props): JSX.Element {
+function GlobalMessages({ id = 'default-message' }: Props): JSX.Element {
     const pathname = usePathname()
     const autoCloseTime = 7000
     const mounted = useRef(false)
@@ -43,28 +43,27 @@ function GlobalMessages({ id = 'default-message'}: Props): JSX.Element {
     useEffect(() => {
         mounted.current = true
         // subscribe to new message notifications
-        const subscription = MessageService.enableSubscribing(id)
-            .subscribe(message => {
-                // clear message when an empty text is received
-                if (CommonUtils.isNullEmptyOrUndefinedString(message.text)) {
-                    setMessages(messages => {
-                        // filter out messages without 'keepAfterRouteChange' flag
-                        const filteredMessages = messages.filter(m => m.keepAfterRouteChange === true);
-                            
-                        // remove 'keepAfterRouteChange' flag on the rest
-                        return omit(filteredMessages, 'keepAfterRouteChange')
-                    });
-                } else {
-                    // add message to array with unique id
-                    message.itemId = Math.random();
-                    setMessages(messages => ([...messages, message]))
+        const subscription = MessageService.enableSubscribing(id).subscribe((message) => {
+            // clear message when an empty text is received
+            if (CommonUtils.isNullEmptyOrUndefinedString(message.text)) {
+                setMessages((messages) => {
+                    // filter out messages without 'keepAfterRouteChange' flag
+                    const filteredMessages = messages.filter((m) => m.keepAfterRouteChange === true)
 
-                    // auto close message if required
-                    if (message.autoClose === true) {
-                        setTimeout(() => removeMessage(message), autoCloseTime)
-                    }
+                    // remove 'keepAfterRouteChange' flag on the rest
+                    return omit(filteredMessages, 'keepAfterRouteChange')
+                })
+            } else {
+                // add message to array with unique id
+                message.itemId = Math.random()
+                setMessages((messages) => [...messages, message])
+
+                // auto close message if required
+                if (message.autoClose === true) {
+                    setTimeout(() => removeMessage(message), autoCloseTime)
                 }
-            })
+            }
+        })
 
         // clean up function that runs when the component unmounts
         return () => {
@@ -73,9 +72,7 @@ function GlobalMessages({ id = 'default-message'}: Props): JSX.Element {
             // unsubscribe to avoid memory leaks
             subscription.unsubscribe()
             MessageService.clear(id)
-        };
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        }
     }, [pathname])
 
     function omit(arr: Array<Message>, key: keyof Message) {
@@ -86,22 +83,26 @@ function GlobalMessages({ id = 'default-message'}: Props): JSX.Element {
 
     function removeMessage(message: Message) {
         if (!mounted.current) return
-        setMessages(messages => messages.filter(m => m.itemId !== message.itemId))
-    };
+        setMessages((messages) => messages.filter((m) => m.itemId !== message.itemId))
+    }
 
     return (
         <div className='global-message'>
-            {messages.map((message, index) =>
-                <Alert key={index} variant={message.type} onClose={() => removeMessage(message)} dismissible>
+            {messages.map((message, index) => (
+                <Alert
+                    key={index}
+                    variant={message.type}
+                    onClose={() => removeMessage(message)}
+                    dismissible
+                >
                     <span>
-                        <MessageIcon messageType={message.type} /> {' '}
-                        <strong>{message.lead}: </strong>
+                        <MessageIcon messageType={message.type} /> <strong>{message.lead}: </strong>
                         {message.text}
                     </span>
                 </Alert>
-            )}
+            ))}
         </div>
-    );
+    )
 }
 
 export default GlobalMessages
