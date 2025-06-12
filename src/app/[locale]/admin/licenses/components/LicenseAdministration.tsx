f4890725c4dc67549b53835f780c15509838ab01
deo002 <oberoidearsh@gmail.com>
2025-06-12 18:03:53 +0530

    feat(import): Add osadl and spdx import in admin license page

--- src/app/[locale]/admin/licenses/components/LicenseAdministration.tsx
+++ src/app/[locale]/admin/licenses/components/LicenseAdministration.tsx
@@ -10,13 +10,154 @@
 'use client'
 
 import { StatusCodes } from 'http-status-codes'
-import { getSession, signOut, useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
-import { ReactNode, useEffect, useRef, useState } from 'react'
+import { Dispatch, JSX, ReactNode, SetStateAction, useEffect, useRef, useState } from 'react'
+import { Alert, Modal } from 'react-bootstrap'
+import { FaRegQuestionCircle } from 'react-icons/fa'
+import { ErrorDetails } from '@/object-types'
 import DownloadService from '@/services/download.service'
 import MessageService from '@/services/message.service'
 import { ApiUtils } from '@/utils'
 import DeleteAllLicenseInformationModal from './DeleteAllLicenseInformationModal'
+
+type ModalType = 'OSADL' | 'SPDX' | undefined
+
+interface AlertData {
+    variant: string
+    message: JSX.Element
+}
+
+interface ImportResult {
+    requestStatus: string
+    totalAffectedElements: number
+    totalElements: number
+    message: string
+}
+
+function ConfirmationModal({ type, setType }: { type: ModalType; setType: Dispatch<SetStateAction<ModalType>> }) {
+    const t = useTranslations('default')
+    const purpose = type === 'OSADL' ? t('Import OSADL license obligations') : t('Import SPDX licenses')
+    const confirmation =
+        type === 'OSADL'
+            ? t('Do you really want to import all OSADL license obligations')
+            : t('Do you really want to import all SPDX all licenses')
+    const url = `licenses/import/${type}`
+    enum ImportState {
+        IMPORT,
+        LOADING,
+        DONE,
+    }
+    const [state, setState] = useState<ImportState>(ImportState.IMPORT)
+    const session = useSession()
+    const [alert, setAlert] = useState<AlertData | null>(null)
+
+    useEffect(() => {
+        if (session.status === 'unauthenticated') {
+            signOut()
+        }
+    }, [
+        session,
+    ])
+
+    const handleImport = async () => {
+        try {
+            setState(ImportState.LOADING)
+            if (!session.data) {
+                return signOut()
+            }
+            const response = await ApiUtils.POST(url, {}, session.data.user.access_token)
+            const res = (await response.json()) as ImportResult
+            if (response.status !== StatusCodes.OK) {
+                setState(ImportState.IMPORT)
+            } else {
+                setState(ImportState.DONE)
+            }
+            setAlert({
+                variant: 'success',
+                message: (
+                    <>
+                        <p>{res.message}</p>
+                        <ul>
+                            <li>
+                                {t('Total Affected Elements')}: {res.totalAffectedElements}
+                            </li>
+                            <li>
+                                {t('Total Elements')}: {res.totalElements}
+                            </li>
+                        </ul>
+                    </>
+                ),
+            })
+        } catch (error: unknown) {
+            if (error instanceof DOMException && error.name === 'AbortError') {
+                return
+            }
+            const message = error instanceof Error ? error.message : String(error)
+            MessageService.error(message)
+        }
+    }
+
+    const close = () => {
+        setType(undefined)
+        setState(ImportState.IMPORT)
+        setAlert(null)
+    }
+
+    return (
+        <Modal
+            show={type !== undefined}
+            onHide={close}
+            backdrop='static'
+            centered
+            size='lg'
+        >
+            <Modal.Header>
+                <Modal.Title>
+                    <FaRegQuestionCircle /> {purpose}?{' '}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {confirmation}?
+                {alert && (
+                    <Alert
+                        variant={alert.variant}
+                        id='updateRelease.message.alert'
+                    >
+                        {alert.message}
+                    </Alert>
+                )}
+            </Modal.Body>
+            <Modal.Footer className='justify-content-end'>
+                {state === ImportState.DONE ? (
+                    <button
+                        className='btn btn-primary'
+                        onClick={close}
+                    >
+                        {t('Close')}
+                    </button>
+                ) : (
+                    <button
+                        className='btn btn-dark'
+                        onClick={close}
+                        disabled={state === ImportState.LOADING}
+                    >
+                        {t('Cancel')}
+                    </button>
+                )}
+                {state !== ImportState.DONE && (
+                    <button
+                        className='btn btn-primary'
+                        onClick={() => void handleImport()}
+                        disabled={state === ImportState.LOADING}
+                    >
+                        {purpose}{' '}
+                    </button>
+                )}
+            </Modal.Footer>
+        </Modal>
+    )
+}
 
 export default function LicenseAdministration(): ReactNode {
     const t = useTranslations('default')
@@ -25,14 +166,15 @@ export default function LicenseAdministration(): ReactNode {
     const [overwriteIfExternalIdMatches, setOverwriteIfExternalIdMatches] = useState(false)
     const [overwriteIfIdMatchesEvenWithoutExternalIdMatch, setOverwriteIfIdMatchesEvenWithoutExternalIdMatch] =
         useState(false)
-    const { status } = useSession()
+    const [confirmationModalType, setConfirmationModalType] = useState<ModalType>(undefined)
+    const session = useSession()
 
     useEffect(() => {
-        if (status === 'unauthenticated') {
+        if (session.status === 'unauthenticated') {
             signOut()
         }
     }, [
-        status,
+        session,
     ])
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -52,17 +194,17 @@ export default function LicenseAdministration(): ReactNode {
             const formData = new FormData()
             formData.append('licenseFile', file.current, file.current.name)
 
-            const session = await getSession()
-            if (!session) {
+            if (!session.data) {
                 return signOut()
             }
             const response = await ApiUtils.POST(
-                `licenses/upload?overwriteIfExternalIdMatches=${overwriteIfExternalIdMatches
+                `licenses/upload?overwriteIfExternalIdMatches=${
+                    overwriteIfExternalIdMatches
                 }&overwriteIfIdMatchesEvenWithoutExternalIdMatch=${overwriteIfIdMatchesEvenWithoutExternalIdMatch}`,
                 formData,
-                session.user.access_token,
+                session.data.user.access_token,
             )
-            if (response.status === HttpStatus.OK) {
+            if (response.status === StatusCodes.OK) {
                 MessageService.success(t('Licenses uploaded successfully'))
             } else {
                 const err = (await response.json()) as ErrorDetails
@@ -77,11 +219,10 @@ export default function LicenseAdministration(): ReactNode {
         }
     }
 
-    const downloadLicenseArchive = async () => {
+    const downloadLicenseArchive = () => {
         try {
-            const session = await getSession()
-            if (!session) return signOut()
-            DownloadService.download('licenses/downloadLicenses', session, `LicensesBackup.lics`)
+            if (!session.data) return signOut()
+            void DownloadService.download('licenses/downloadLicenses', session.data, `LicensesBackup.lics`)
         } catch (error) {
             if (error instanceof DOMException && error.name === 'AbortError') {
                 return
@@ -97,25 +238,31 @@ export default function LicenseAdministration(): ReactNode {
                 show={deleteAllLicenseInformationModal}
                 setShow={showDeleteAllLicenseInformationModal}
             />
+            <ConfirmationModal
+                type={confirmationModalType}
+                setType={setConfirmationModalType}
+            />
             <div className='mt-4 mx-5'>
                 <div className='row'>
                     <div className='col-lg-8'>
                         <button
                             type='button'
                             className='btn btn-primary col-auto me-2'
-                            onClick={() => downloadLicenseArchive()}
+                            onClick={() => void downloadLicenseArchive()}
                         >
                             {t('Download License Archive')}
                         </button>
                         <button
                             type='button'
                             className='btn btn-primary col-auto me-2'
+                            onClick={() => setConfirmationModalType('SPDX')}
                         >
                             {t('Import SPDX Information')}
                         </button>
                         <button
                             type='button'
                             className='btn btn-primary col-auto me-2'
+                            onClick={() => setConfirmationModalType('OSADL')}
                         >
                             {t('Import OSADL Information')}
                         </button>
@@ -177,7 +324,7 @@ export default function LicenseAdministration(): ReactNode {
                     <button
                         type='button'
                         className='btn btn-secondary col-auto mt-3'
-                        onClick={() => uploadLicenses()}
+                        onClick={() => void uploadLicenses()}
                     >
                         {t('Upload Licenses')}
                     </button>
