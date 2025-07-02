3c2b37a2e1505f6550a9adc8473494f4f2214781
amritkv <er.akverma8@gmail.com>
2025-07-02 12:08:43 +0530

    fix(Admin): Added missing external Id to user detail and edit page

--- src/app/[locale]/admin/users/details/[id]/page.tsx
+++ src/app/[locale]/admin/users/details/[id]/page.tsx
@@ -1,5 +1,6 @@
 // Copyright (C) TOSHIBA CORPORATION, 2025. Part of the SW360 Frontend Project.
 // Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2025. Part of the SW360 Frontend Project.
+// Copyright (C) Siemens AG, 2025. Part of the SW360 Frontend Project.
 
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
@@ -68,6 +69,10 @@ const UserDetailPage = (): JSX.Element => {
                     <tr>
                         <td>{t('Email')}:</td>
                         <td>{user.email}</td>
+                    </tr>
+                    <tr>
+                        <td>{t('Global Identifier')}:</td>
+                        <td>{user.externalid}</td>
                     </tr>
                     <tr>
                         <td>{t('Active status')}:</td>
