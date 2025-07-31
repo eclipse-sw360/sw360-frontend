36416b1c92fab12fcd8625e88c771d503c300edc
deo002 <oberoidearsh@gmail.com>
2025-07-31 15:31:04 +0530

    feat(home): Migrate my projects and my components table

--- src/app/[locale]/home/components/MyProjectsWidget.tsx
+++ src/app/[locale]/home/components/MyProjectsWidget.tsx
@@ -8,124 +8,229 @@
 // License-Filename: LICENSE
 
 import LicenseClearing from '@/components/LicenseClearing'
+import MessageService from '@/services/message.service'
+import { ColumnDef, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from '@tanstack/react-table'
 import { getSession, signOut } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
-import { useSearchParams } from 'next/navigation'
-import { ReactNode, useCallback, useEffect, useState, type JSX } from 'react'
+import { ReactNode, useEffect, useMemo, useState } from 'react'
 import { Spinner } from 'react-bootstrap'
 
 import { HttpStatus } from '@/object-types'
 import { ApiUtils, CommonUtils } from '@/utils'
-import { Table, _ } from 'next-sw360'
-
+import { SW360Table, TableFooter } from 'next-sw360'
 import Link from 'next/link'
 
-import { Embedded, Project } from '@/object-types'
+import { Embedded, ErrorDetails, PageableQueryParam, PaginationMeta, Project } from '@/object-types'
 import HomeTableHeader from './HomeTableHeader'
 
 type EmbeddedProjects = Embedded<Project, 'sw360:projects'>
 
-function MyProjectsWidget(): ReactNode {
-    const [data, setData] = useState<Array<(string | JSX.Element)[]>>([])
+export default function MyProjectsWidget(): ReactNode {
     const t = useTranslations('default')
-    const params = useSearchParams()
-    const [loading, setLoading] = useState(true)
     const [reload, setReload] = useState(false)
 
-    const fetchData = useCallback(async (queryUrl: string, signal: AbortSignal) => {
-        const session = await getSession()
-        if (CommonUtils.isNullOrUndefined(session)) return signOut()
-        const response = await ApiUtils.GET(queryUrl, session.user.access_token, signal)
-        if (response.status === HttpStatus.OK) {
-            const myprojects = (await response.json()) as EmbeddedProjects
-            return myprojects
-        } else {
-            return undefined
-        }
-    }, [])
+    const columns = useMemo<ColumnDef<Project>[]>(
+        () => [
+            {
+                id: 'name',
+                accessorKey: 'name',
+                header: t('Project Name'),
+                enableSorting: true,
+                cell: ({ row }) => {
+                    const { name, version } = row.original
+                    const id = row.original['_links']['self']['href'].split('/').at(-1)
+                    return (
+                        <Link
+                            href={`/projects/detail/${id}`}
+                            className='text-link'
+                        >
+                            {name} {!CommonUtils.isNullEmptyOrUndefinedString(version) && `(${version})`}
+                        </Link>
+                    )
+                },
+                meta: {
+                    width: '17.5%',
+                },
+            },
+            {
+                id: 'description',
+                header: t('Description'),
+                accessorKey: 'description',
+                cell: (info) => info.getValue(),
+                meta: {
+                    width: '32.5%',
+                },
+            },
+            {
+                id: 'approvedReleases',
+                header: t('Approved Releases'),
+                enableSorting: false,
+                cell: ({ row }) => {
+                    const id = row.original['_links']['self']['href'].split('/').at(-1)
+                    return <>{id && <LicenseClearing projectId={id} />}</>
+                },
+                meta: {
+                    width: '10%',
+                },
+            },
+        ],
+        [t],
+    )
+
+    const [pageableQueryParam, setPageableQueryParam] = useState<PageableQueryParam>({
+        page: 0,
+        page_entries: 5,
+        sort: '',
+    })
+    const [paginationMeta, setPaginationMeta] = useState<PaginationMeta | undefined>({
+        size: 0,
+        totalElements: 0,
+        totalPages: 0,
+        number: 0,
+    })
+    const [projectData, setProjectData] = useState<Project[]>(() => [])
+    const memoizedData = useMemo(() => projectData, [projectData])
+    const [showProcessing, setShowProcessing] = useState(false)
 
     useEffect(() => {
-        setLoading(true)
-        const searchParams = Object.fromEntries(params)
-        const queryUrl = CommonUtils.createUrlWithParams('projects/myprojects', searchParams)
-
         const controller = new AbortController()
         const signal = controller.signal
 
-        fetchData(queryUrl, signal)
-            .then((projects: EmbeddedProjects | undefined) => {
-                if (projects === undefined) {
+        const timeLimit = projectData.length !== 0 ? 400 : 0
+        const timeout = setTimeout(() => {
+            setShowProcessing(true)
+        }, timeLimit)
+
+        void (async () => {
+            try {
+                const session = await getSession()
+                if (CommonUtils.isNullOrUndefined(session)) return signOut()
+
+                const queryUrl = CommonUtils.createUrlWithParams(
+                    `projects/myprojects`,
+                    Object.fromEntries(Object.entries(pageableQueryParam).map(([key, value]) => [key, String(value)])),
+                )
+                const response = await ApiUtils.GET(queryUrl, session.user.access_token, signal)
+                if (response.status !== HttpStatus.OK) {
+                    const err = (await response.json()) as ErrorDetails
+                    throw new Error(err.message)
+                }
+
+                const data = (await response.json()) as EmbeddedProjects
+                setPaginationMeta(data.page)
+                setProjectData(
+                    CommonUtils.isNullOrUndefined(data['_embedded']['sw360:projects'])
+                        ? []
+                        : data['_embedded']['sw360:projects'],
+                )
+            } catch (error) {
+                if (error instanceof DOMException && error.name === 'AbortError') {
                     return
                 }
+                const message = error instanceof Error ? error.message : String(error)
+                MessageService.error(message)
+            } finally {
+                clearTimeout(timeout)
+                setShowProcessing(false)
+            }
+        })()
 
-                if (!CommonUtils.isNullOrUndefined(projects['_embedded']['sw360:projects'])) {
-                    setData(
-                        projects['_embedded']['sw360:projects'].map((item: Project) => [
-                            `${item.name}|${CommonUtils.getIdFromUrl(item._links.self.href)}|${CommonUtils.isNullEmptyOrUndefinedString(item.version) && `(${item.version})`}`,
-                            CommonUtils.truncateText(item.description ?? '', 40),
-                            item.id ?? '',
-                        ]),
-                    )
+        return () => controller.abort()
+    }, [pageableQueryParam, reload])
+
+    const table = useReactTable({
+        data: memoizedData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+
+        // table state config
+        state: {
+            pagination: {
+                pageIndex: pageableQueryParam.page,
+                pageSize: pageableQueryParam.page_entries,
+            },
+            sorting: [
+                {
+                    id: pageableQueryParam.sort.split(',')[0],
+                    desc: pageableQueryParam.sort.split(',')[1] === 'desc',
+                },
+            ],
+        },
+
+        // server side sorting config
+        manualSorting: true,
+        getSortedRowModel: getSortedRowModel(),
+        onSortingChange: (updater) => {
+            setPageableQueryParam((prev) => {
+                const prevSorting: SortingState = [
+                    {
+                        id: prev.sort.split(',')[0],
+                        desc: prev.sort.split(',')[1] === 'desc',
+                    },
+                ]
+
+                const nextSorting = typeof updater === 'function' ? updater(prevSorting) : updater
+
+                if (nextSorting.length > 0) {
+                    const { id, desc } = nextSorting[0]
+                    return {
+                        ...prev,
+                        sort: `${id},${desc ? 'desc' : 'asc'}`,
+                    }
+                }
+
+                return {
+                    ...prev,
+                    sort: '',
                 }
             })
-            .catch((err: Error) => {
-                throw new Error(err.message)
-            })
-            .finally(() => {
-                setLoading(false)
-            })
-
-        return () => {
-            controller.abort()
-        }
-    }, [fetchData, params, reload])
-
-    const title = t('My Projects')
-    const columns = [
-        {
-            id: 'Project Name',
-            name: t('Project Name'),
-            formatter: (cell: string) => {
-                const item = cell.split('|')
-                return _(
-                    <Link href={'projects/detail/' + item[1]}>
-                        {item[0]} {item[2]}
-                    </Link>,
-                )
-            },
         },
-        t('Description'),
-        {
-            id: 'License Clearing',
-            name: t('License Clearing'),
-            formatter: (cell: string) => {
-                return _(<LicenseClearing projectId={cell} />)
-            },
+
+        // server side pagination config
+        manualPagination: true,
+        pageCount: paginationMeta?.totalPages ?? 1,
+        onPaginationChange: (updater) => {
+            const next =
+                typeof updater === 'function'
+                    ? updater({
+                          pageIndex: pageableQueryParam.page,
+                          pageSize: pageableQueryParam.page_entries,
+                      })
+                    : updater
+
+            setPageableQueryParam((prev) => ({
+                ...prev,
+                page: next.pageIndex + 1,
+                page_entries: next.pageSize,
+            }))
         },
-    ]
-    const language = { noRecordsFound: t('NoProjectsFound') }
+    })
 
     return (
         <div>
             <HomeTableHeader
-                title={title}
+                title={t('My Projects')}
                 setReload={setReload}
             />
-            {loading == false ? (
-                <Table
-                    columns={columns}
-                    data={data}
-                    pagination={{ limit: 5 }}
-                    selector={false}
-                    language={language}
-                />
-            ) : (
-                <div className='col-12'>
-                    <Spinner className='spinner' />
-                </div>
-            )}
+            <div className='mb-3'>
+                {pageableQueryParam && table && paginationMeta ? (
+                    <>
+                        <SW360Table
+                            table={table}
+                            showProcessing={showProcessing}
+                        />
+                        <TableFooter
+                            pageableQueryParam={pageableQueryParam}
+                            setPageableQueryParam={setPageableQueryParam}
+                            paginationMeta={paginationMeta}
+                        />
+                    </>
+                ) : (
+                    <div className='col-12 mt-1 text-center'>
+                        <Spinner className='spinner' />
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
-
-export default MyProjectsWidget
