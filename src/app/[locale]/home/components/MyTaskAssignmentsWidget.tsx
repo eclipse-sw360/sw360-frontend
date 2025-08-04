f70d37bf34e77ce657e824345395c5e28aac788f
deo002 <oberoidearsh@gmail.com>
2025-08-04 15:11:17 +0530

    refactor(mytaskassignments): Migrate my task assignments table to tanstack table

--- src/app/[locale]/home/components/MyTaskAssignmentsWidget.tsx
+++ src/app/[locale]/home/components/MyTaskAssignmentsWidget.tsx
@@ -8,13 +8,21 @@
 // SPDX-License-Identifier: EPL-2.0
 // License-Filename: LICENSE
 
-import { Embedded, HttpStatus, ModerationRequest } from '@/object-types'
+import {
+    Embedded,
+    ErrorDetails,
+    HttpStatus,
+    ModerationRequest,
+    PageableQueryParam,
+    PaginationMeta,
+} from '@/object-types'
+import MessageService from '@/services/message.service'
 import { ApiUtils, CommonUtils } from '@/utils/index'
+import { ColumnDef, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 import { getSession, signOut } from 'next-auth/react'
 import { useTranslations } from 'next-intl'
-import { Table, _ } from 'next-sw360'
-import Link from 'next/link'
-import React, { ReactNode, useCallback, useEffect, useState, type JSX } from 'react'
+import { SW360Table, TableFooter } from 'next-sw360'
+import { ReactNode, useEffect, useMemo, useState } from 'react'
 import { Spinner } from 'react-bootstrap'
 import HomeTableHeader from './HomeTableHeader'
 
@@ -24,9 +32,7 @@ interface TaskAssignmentStatusMap {
 }
 
 function MyTaskAssignmentsWidget(): ReactNode {
-    const [taskAssignmentData, setTaskAssignmentData] = useState<Array<(string | JSX.Element)[]>>([])
     const t = useTranslations('default')
-    const [loading, setLoading] = useState(true)
     const [reload, setReload] = useState(false)
     const taskAssignmentStatus: TaskAssignmentStatusMap = {
         INPROGRESS: t('In Progress'),
@@ -35,81 +41,161 @@ function MyTaskAssignmentsWidget(): ReactNode {
         REJECTED: t('REJECTED'),
     }
 
-    const fetchData = useCallback(async (url: string) => {
-        const session = await getSession()
-        if (CommonUtils.isNullOrUndefined(session)) return signOut()
-        const response = await ApiUtils.GET(url, session.user.access_token)
-        if (response.status == HttpStatus.OK) {
-            const data = (await response.json()) as EmbeddedTaskAssignments
-            return data
-        } else if (response.status == HttpStatus.UNAUTHORIZED) {
-            return signOut()
-        } else {
-            return {
-                _embedded: {
-                    'sw360:moderationRequests': [],
+    const columns = useMemo<ColumnDef<ModerationRequest>[]>(
+        () => [
+            {
+                id: 'name',
+                header: t('Document Name'),
+                enableSorting: true,
+                cell: ({ row }) => {
+                    const { id, documentName } = row.original
+                    return <>{`${id}|${documentName}`}</>
                 },
-            }
-        }
-    }, [])
+                meta: {
+                    width: '60%',
+                },
+            },
+            {
+                id: 'status',
+                header: t('Status'),
+                cell: ({ row }) => {
+                    const { moderationState } = row.original
+                    return <>{taskAssignmentStatus[moderationState ?? 'INPROGRESS']}</>
+                },
+                meta: {
+                    width: '40%',
+                },
+            },
+        ],
+        [t],
+    )
+
+    const [pageableQueryParam, setPageableQueryParam] = useState<PageableQueryParam>({
+        page: 0,
+        page_entries: 5,
+        sort: '',
+    })
+    const [paginationMeta, setPaginationMeta] = useState<PaginationMeta | undefined>({
+        size: 0,
+        totalElements: 0,
+        totalPages: 0,
+        number: 0,
+    })
+    const [taskAssignmentData, setTaskAssignmentData] = useState<ModerationRequest[]>(() => [])
+    const memoizedData = useMemo(() => taskAssignmentData, [taskAssignmentData])
+    const [showProcessing, setShowProcessing] = useState(false)
 
     useEffect(() => {
-        setLoading(true)
-        void fetchData('moderationrequest/byState?state=open&allDetails=false')
-            .then((moderationRequests: EmbeddedTaskAssignments | undefined) => {
-                if (moderationRequests === undefined) {
+        const controller = new AbortController()
+        const signal = controller.signal
+
+        const timeLimit = taskAssignmentData.length !== 0 ? 400 : 0
+        const timeout = setTimeout(() => {
+            setShowProcessing(true)
+        }, timeLimit)
+
+        void (async () => {
+            try {
+                const session = await getSession()
+                if (CommonUtils.isNullOrUndefined(session)) return signOut()
+
+                const queryUrl = CommonUtils.createUrlWithParams(
+                    `components/mycomponents`,
+                    Object.fromEntries(Object.entries(pageableQueryParam).map(([key, value]) => [key, String(value)])),
+                )
+                const response = await ApiUtils.GET(queryUrl, session.user.access_token, signal)
+                if (response.status !== HttpStatus.OK) {
+                    const err = (await response.json()) as ErrorDetails
+                    throw new Error(err.message)
+                }
+
+                const data = (await response.json()) as EmbeddedTaskAssignments
+                setPaginationMeta(data.page)
+                setTaskAssignmentData(
+                    CommonUtils.isNullOrUndefined(data['_embedded']?.['sw360:moderationRequests'])
+                        ? []
+                        : data['_embedded']['sw360:moderationRequests'],
+                )
+            } catch (error) {
+                if (error instanceof DOMException && error.name === 'AbortError') {
                     return
                 }
-                if (!CommonUtils.isNullOrUndefined(moderationRequests['_embedded']['sw360:moderationRequests'])) {
-                    setTaskAssignmentData(
-                        moderationRequests['_embedded']['sw360:moderationRequests'].map((item: ModerationRequest) => [
-                            `${item.id}|${item.documentName}`,
-                            taskAssignmentStatus[item.moderationState ?? 'INPROGRESS'],
-                        ]),
-                    )
-                }
-            })
-            .catch((err: Error) => {
-                throw new Error(err.message)
-            })
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [fetchData, reload])
+                const message = error instanceof Error ? error.message : String(error)
+                MessageService.error(message)
+            } finally {
+                clearTimeout(timeout)
+                setShowProcessing(false)
+            }
+        })()
 
-    const title = t('My Task Assignments')
-    const columns = [
-        {
-            id: 'Document Name',
-            name: t('Document Name'),
-            formatter: (cell: string) => {
-                const [id, documentName] = cell.split('|')
-                return _(<Link href={'moderationrequest/' + id}>{documentName}</Link>)
+        return () => controller.abort()
+    }, [pageableQueryParam, reload])
+
+    const table = useReactTable({
+        data: memoizedData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+
+        // table state config
+        state: {
+            pagination: {
+                pageIndex: pageableQueryParam.page,
+                pageSize: pageableQueryParam.page_entries,
             },
+            sorting: [
+                {
+                    id: pageableQueryParam.sort.split(',')[0],
+                    desc: pageableQueryParam.sort.split(',')[1] === 'desc',
+                },
+            ],
         },
-        t('Status'),
-    ]
-    const language = { noRecordsFound: t('NoTasksAssigned') }
+
+        // server side pagination config
+        manualPagination: true,
+        pageCount: paginationMeta?.totalPages ?? 1,
+        onPaginationChange: (updater) => {
+            const next =
+                typeof updater === 'function'
+                    ? updater({
+                          pageIndex: pageableQueryParam.page,
+                          pageSize: pageableQueryParam.page_entries,
+                      })
+                    : updater
+
+            setPageableQueryParam((prev) => ({
+                ...prev,
+                page: next.pageIndex + 1,
+                page_entries: next.pageSize,
+            }))
+        },
+    })
 
     return (
         <div>
             <HomeTableHeader
-                title={title}
+                title={t('My Task Assignments')}
                 setReload={setReload}
             />
-            {loading == false ? (
-                <Table
-                    columns={columns}
-                    data={taskAssignmentData}
-                    pagination={{ limit: 5 }}
-                    selector={false}
-                    language={language}
-                />
-            ) : (
-                <div className='col-12'>
-                    <Spinner className='spinner' />
-                </div>
-            )}
+            <div className='mb-3'>
+                {pageableQueryParam && table && paginationMeta ? (
+                    <>
+                        <SW360Table
+                            table={table}
+                            showProcessing={showProcessing}
+                            noRecordsFoundMessage={t('NoTasksAssigned')}
+                        />
+                        <TableFooter
+                            pageableQueryParam={pageableQueryParam}
+                            setPageableQueryParam={setPageableQueryParam}
+                            paginationMeta={paginationMeta}
+                        />
+                    </>
+                ) : (
+                    <div className='col-12 mt-1 text-center'>
+                        <Spinner className='spinner' />
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
