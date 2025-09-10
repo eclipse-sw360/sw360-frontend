fd40971f061f23253a1f6779d87f4d2bfe7c4a3a
deo002 <oberoidearsh@gmail.com>
2025-09-10 10:51:38 +0530

    refactor(search): Migrate search table to tanstack

--- src/app/[locale]/search/components/SearchPage.tsx
+++ src/app/[locale]/search/components/SearchPage.tsx
@@ -10,113 +10,231 @@
 'use client'
 
 import Link from 'next/link'
-import { ReactNode, useState } from 'react'
+import { ReactNode, useMemo, useState } from 'react'
 import { Alert, Spinner } from 'react-bootstrap'
 
-import { SearchResult } from '@/object-types'
+import { PageableQueryParam, PaginationMeta, SearchResult } from '@/object-types'
+import { ColumnDef, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 import { useTranslations } from 'next-intl'
-import { _, Table } from 'next-sw360'
+import { PageSizeSelector, SW360Table, TableFooter } from 'next-sw360'
 import KeywordSearch from './KeywordSearch'
 
 export default function Search(): ReactNode {
     const t = useTranslations('default')
-    const [data, setData] = useState<SearchResult[] | null>([])
 
-    const columns = [
-        {
-            id: 'searchResults.type',
-            name: t('Type'),
-            width: '6%',
-            formatter: (type: string) =>
-                _(
-                    <>
-                        {(() => {
-                            if (type === 'package') {
-                                return (
-                                    <svg
-                                        className='project_icon mb-1'
-                                        height={18}
-                                        width={18}
-                                    >
-                                        <use href='icons.svg#project'></use>
-                                    </svg>
-                                )
-                            } else {
-                                return (
-                                    <svg
-                                        className={`${type}_icon mb-1`}
-                                        height={18}
-                                        width={18}
-                                    >
-                                        <use href={`icons.svg#${type}`}></use>
-                                    </svg>
-                                )
-                            }
-                        })()}
-                    </>,
-                ),
-            sort: true,
+    const columns = useMemo<ColumnDef<SearchResult>[]>(
+        () => [
+            {
+                id: 'type',
+                header: t('Type'),
+                cell: ({ row }) => {
+                    const { type } = row.original
+                    return (
+                        <div className='text-center'>
+                            {(() => {
+                                if (type === 'package') {
+                                    return (
+                                        <svg
+                                            className='package_icon mb-1'
+                                            height={18}
+                                            width={18}
+                                        >
+                                            <use href='icons.svg#project'></use>
+                                        </svg>
+                                    )
+                                } else {
+                                    return (
+                                        <svg
+                                            className={`${type}_icon mb-1`}
+                                            height={18}
+                                            width={18}
+                                        >
+                                            <use href={`icons.svg#${type}`}></use>
+                                        </svg>
+                                    )
+                                }
+                            })()}
+                        </div>
+                    )
+                },
+                meta: {
+                    width: '10%',
+                },
+            },
+            {
+                id: 'text',
+                header: t('Text'),
+                cell: ({ row }) => {
+                    const { id, type, name } = row.original
+                    return (
+                        <>
+                            {(() => {
+                                if (type === 'project') {
+                                    return (
+                                        <Link
+                                            href={`/projects/detail/${id}`}
+                                            className='text-link'
+                                        >
+                                            {name}
+                                        </Link>
+                                    )
+                                } else if (type === 'release') {
+                                    return (
+                                        <Link
+                                            href={`/components/releases/detail/${id}`}
+                                            className='text-link'
+                                        >
+                                            {name}
+                                        </Link>
+                                    )
+                                } else if (type === 'component') {
+                                    return (
+                                        <Link
+                                            href={`/components/detail/${id}`}
+                                            className='text-link'
+                                        >
+                                            {name}
+                                        </Link>
+                                    )
+                                } else if (type === 'license') {
+                                    return (
+                                        <Link
+                                            href={`/licenses/detail/?id=${id}`}
+                                            className='text-link'
+                                        >
+                                            {name}
+                                        </Link>
+                                    )
+                                } else if (type === 'package') {
+                                    return (
+                                        <Link
+                                            href={`/packages/detail/${id}`}
+                                            className='text-link'
+                                        >
+                                            {name}
+                                        </Link>
+                                    )
+                                } else if (type === 'obligation') {
+                                    return (
+                                        <Link
+                                            href={`/obligations/detail/${id}`}
+                                            className='text-link'
+                                        >
+                                            {name}
+                                        </Link>
+                                    )
+                                } else if (type === 'vendor') {
+                                    return <p>{name}</p>
+                                } else if (type === 'user') {
+                                    return <p>{name}</p>
+                                }
+                            })()}
+                        </>
+                    )
+                },
+                meta: {
+                    width: '90%',
+                },
+            },
+        ],
+        [t],
+    )
+    const [pageableQueryParam, setPageableQueryParam] = useState<PageableQueryParam>({
+        page: 0,
+        page_entries: 10,
+        sort: '',
+    })
+    const [paginationMeta, setPaginationMeta] = useState<PaginationMeta | undefined>({
+        size: 0,
+        totalElements: 0,
+        totalPages: 0,
+        number: 0,
+    })
+    const [searchData, setSearchData] = useState<SearchResult[]>(() => [])
+    const memoizedData = useMemo(() => searchData, [searchData])
+    const [showProcessing, setShowProcessing] = useState(false)
+
+    const table = useReactTable({
+        data: memoizedData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+
+        // table state config
+        state: {
+            pagination: {
+                pageIndex: pageableQueryParam.page,
+                pageSize: pageableQueryParam.page_entries,
+            },
+            sorting: [
+                {
+                    id: pageableQueryParam.sort.split(',')[0],
+                    desc: pageableQueryParam.sort.split(',')[1] === 'desc',
+                },
+            ],
         },
-        {
-            id: 'searchResults.text',
-            name: t('Text'),
-            formatter: ({ id, type, name }: { id: string; type: string; name: string }) =>
-                _(
-                    <>
-                        {(() => {
-                            if (type === 'project') {
-                                return <Link href={`/projects/detail/${id}`}>{name}</Link>
-                            } else if (type === 'release') {
-                                return <Link href={`/components/releases/detail/${id}`}>{name}</Link>
-                            } else if (type === 'component') {
-                                return <Link href={`/components/detail/${id}`}>{name}</Link>
-                            } else if (type === 'license') {
-                                return <Link href={`/licenses/detail/?id=${id}`}>{name}</Link>
-                            } else if (type === 'package') {
-                                return <Link href={`/packages/detail/${id}`}>{name}</Link>
-                            } else if (type === 'obligation') {
-                                return <Link href={`/obligations/detail/${id}`}>{name}</Link>
-                            } else if (type === 'vendor') {
-                                return <p>{name}</p>
-                            } else if (type === 'user') {
-                                return <p>{name}</p>
-                            }
-                        })()}
-                    </>,
-                ),
-            sort: true,
+
+        // server side pagination config
+        manualPagination: true,
+        pageCount: paginationMeta?.totalPages ?? 1,
+        onPaginationChange: (updater) => {
+            const next =
+                typeof updater === 'function'
+                    ? updater({
+                          pageIndex: pageableQueryParam.page,
+                          pageSize: pageableQueryParam.page_entries,
+                      })
+                    : updater
+
+            setPageableQueryParam((prev) => ({
+                ...prev,
+                page: next.pageIndex + 1,
+                page_entries: next.pageSize,
+            }))
         },
-    ]
+    })
 
     return (
         <>
             <div className='container page-content'>
                 <div className='row mt-2'>
                     <div className='col-lg-2'>
-                        <KeywordSearch setData={setData} />
+                        <KeywordSearch
+                            setData={setSearchData}
+                            setShowProcessing={setShowProcessing}
+                            setPaginationMeta={setPaginationMeta}
+                            pageableQueryParam={pageableQueryParam}
+                        />
                     </div>
                     <div className='col'>
                         <div className='row d-flex justify-content-end'>
                             <div className='col text-truncate buttonheader-title'>
-                                {t('SEARCH RESULTS', { datalength: data ? data.length : 0 })}
+                                {t('SEARCH RESULTS')} {paginationMeta?.totalElements ?? 0}
                             </div>
                         </div>
                         <div className='row'>
-                            {!data ? (
-                                <div
-                                    className='col-12'
-                                    style={{ textAlign: 'center' }}
-                                >
-                                    <Spinner className='spinner' />
-                                </div>
-                            ) : (
-                                <Table
-                                    data={data.map((elem) => [elem.type, elem])}
-                                    columns={columns}
-                                    pagination={true}
-                                    selector={true}
-                                />
-                            )}
+                            <div className='mb-3'>
+                                {pageableQueryParam && table && paginationMeta ? (
+                                    <>
+                                        <PageSizeSelector
+                                            pageableQueryParam={pageableQueryParam}
+                                            setPageableQueryParam={setPageableQueryParam}
+                                        />
+                                        <SW360Table
+                                            table={table}
+                                            showProcessing={showProcessing}
+                                        />
+                                        <TableFooter
+                                            pageableQueryParam={pageableQueryParam}
+                                            setPageableQueryParam={setPageableQueryParam}
+                                            paginationMeta={paginationMeta}
+                                        />
+                                    </>
+                                ) : (
+                                    <div className='col-12 mt-1 text-center'>
+                                        <Spinner className='spinner' />
+                                    </div>
+                                )}
+                            </div>
                             <div className='col-lg-8'>
                                 <Alert
                                     variant='warning'
