ab52fa65e643e04f59652000c1719bc9ed177751
manhuu14 <umanhuu@gmail.com>
2025-12-27 22:59:36 +0530

    feat: Add search bar to obligations tab

--- src/app/[locale]/licenses/detail/components/Obligations.tsx
+++ src/app/[locale]/licenses/detail/components/Obligations.tsx
@@ -1,11 +1,9 @@
 // Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
 // Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
 // Copyright (C) Siemens AG, 2025. Part of the SW360 Frontend Project.
-
 // This program and the accompanying materials are made
 // available under the terms of the Eclipse Public License 2.0
 // which is available at https://www.eclipse.org/legal/epl-2.0/
-
 // SPDX-License-Identifier: EPL-2.0
 // License-Filename: LICENSE
 
@@ -291,5 +289,4 @@ const Obligations = ({ licenseId, isEditWhitelist, whitelist, setWhitelist }: Pr
         </div>
     )
 }
-
 export default Obligations
