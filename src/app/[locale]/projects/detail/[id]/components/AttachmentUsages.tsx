a9e40bdba7ce4509912606fa3d43c84dbe5e7ff8
Dearsh Oberoi <oberoidearsh@gmail.com>
2026-01-16 11:25:10 +0530

    fix(aborterrors): Fix abort errors

--- src/app/[locale]/projects/detail/[id]/components/AttachmentUsages.tsx
+++ src/app/[locale]/projects/detail/[id]/components/AttachmentUsages.tsx
@@ -375,7 +375,7 @@ function AttachmentUsagesComponent({ projectId }: { projectId: string }): JSX.El
                     return
                 }
                 const message = error instanceof Error ? error.message : String(error)
-                throw new Error(message)
+                MessageService.error(message)
             } finally {
                 clearTimeout(timeout)
                 setShowProcessingAttachmentUsages(false)
