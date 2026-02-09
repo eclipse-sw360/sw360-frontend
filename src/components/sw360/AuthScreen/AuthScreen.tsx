34c537c11c0fea2e11ff17cdee6e9667445bea04
aaryan359 <aaryanmeena96@gmail.com>
2026-02-10 03:47:51 +0530

    fix: Eye icons same as other code and add translations for different languages

--- src/components/sw360/AuthScreen/AuthScreen.tsx
+++ src/components/sw360/AuthScreen/AuthScreen.tsx
@@ -17,6 +17,7 @@ import { useLocale, useTranslations } from 'next-intl'
 import { LanguageSwitcher, PageSpinner } from 'next-sw360'
 import { ReactNode, useState } from 'react'
 import { Alert, Button, Form, Modal } from 'react-bootstrap'
+import { BsEye, BsEyeSlash } from 'react-icons/bs'
 import { CREDENTIALS, KEYCLOAK_PROVIDER, SW360OAUTH_PROVIDER } from '@/constants'
 import { AUTH_PROVIDER } from '@/utils/env'
 
@@ -163,46 +164,7 @@ function AuthScreen(): ReactNode {
                                     aria-label={showPassword ? t('Hide password') : t('Show password')}
                                     title={showPassword ? t('Hide password') : t('Show password')}
                                 >
-                                    {showPassword ? (
-                                        <svg
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            width='20'
-                                            height='20'
-                                            viewBox='0 0 24 24'
-                                            fill='none'
-                                            stroke='currentColor'
-                                            strokeWidth='2'
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                        >
-                                            <path d='M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z' />
-                                            <circle
-                                                cx='12'
-                                                cy='12'
-                                                r='3'
-                                            />
-                                        </svg>
-                                    ) : (
-                                        <svg
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            width='20'
-                                            height='20'
-                                            viewBox='0 0 24 24'
-                                            fill='none'
-                                            stroke='currentColor'
-                                            strokeWidth='2'
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                        >
-                                            <path d='M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24' />
-                                            <line
-                                                x1='1'
-                                                y1='1'
-                                                x2='23'
-                                                y2='23'
-                                            />
-                                        </svg>
-                                    )}
+                                    {showPassword ? <BsEye /> : <BsEyeSlash />}
                                 </button>
                             </div>
                         </Form.Group>
