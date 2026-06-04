635c379f24a51686dc43f78dcf83056537e526e6
aaryan359 <aaryanmeena96@gmail.com>
2026-06-05 02:17:31 +0530

    fix: Correct Bulk Release Edit link in admin nav list

--- src/object-types/NavList.ts
+++ src/object-types/NavList.ts
@@ -80,7 +80,7 @@ function NavList(): NavItem[] {
                     id: 'admin_vendors',
                 },
                 {
-                    href: '/bulkreleaseedit',
+                    href: '/admin/bulkreleaseedit',
                     name: t('Bulk Release Edit'),
                     id: 'admin_bulk_edit',
                 },
