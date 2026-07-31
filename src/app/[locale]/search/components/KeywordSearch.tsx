5c001e10cf0977d42a286eb3b628b4999dfe31b3
Gaurav Mishra <mishra.gaurav@siemens.com>
2026-07-31 19:21:27 +0530

    fix(search): Enable all filters by default
    Enable all filters in keyword search page by default.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- src/app/[locale]/search/components/KeywordSearch.tsx
+++ src/app/[locale]/search/components/KeywordSearch.tsx
@@ -172,15 +172,15 @@ function KeywordSearch({
     const querySearchText = searchParams.get('searchText')?.trim() ?? ''
 
     const initialState: SEARCH_STATE = {
-        project: false,
-        component: false,
-        license: false,
-        release: false,
-        obligation: false,
-        user: false,
-        vendor: false,
+        project: true,
+        component: true,
+        license: true,
+        release: true,
+        obligation: true,
+        user: true,
+        vendor: true,
         entireDocument: true,
-        package: false,
+        package: true,
     }
 
     const [searchOptions, dispatch] = useReducer(reducer, initialState)
