5e93600cba2f47b53949a1eb339ef02e7277def4
Nikesh Kumar <kumar.nikesh@siemens.com>
2026-08-13 22:01:11 +0530

    fix(UI): Show Used By Projects loader during API fetch
    Signed-off-by: Nikesh Kumar <kumar.nikesh@siemens.com>

--- src/components/ResourcesUsing/ProjectsUsing.tsx
+++ src/components/ResourcesUsing/ProjectsUsing.tsx
@@ -9,6 +9,8 @@
 // SPDX-License-Identifier: EPL-2.0
 // License-Filename: LICENSE
 
+'use client'
+
 import { ColumnDef, getCoreRowModel, useReactTable } from '@tanstack/react-table'
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
